0427417ded68d00e7da6350b13edd3505b00b71e
bibekgyawali11 <bibekgyawali11@users.noreply.github.com>
2019-08-24 10:32:41 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import { Grid } from '@material-ui/core';
+
+import { SearchBar, VideoDetail, VideoList }  from './components';
+
+import youtube from './api/youtube';
+//import color from '@material-ui/core/colors/lightGreen';
+
+class App extends React.Component{
+  state = {
+    videos: [],
+    selectedVideo: null,
+  }
+
+  onVideoSelect =(video) => {
+    this.setState({ selectedVideo: video});
+  }
+  handleSubmit = async (searchTerm) => {
+     const response = await youtube.get('search',{
+        params: {
+            part: 'snippet',
+            maxResults: 7, 
+            key: 'AIzaSyBveyiHqNuFP2HT1UiO2tC1Q-Tpk9ckS3c',
+            q: searchTerm 
+        }
+        });
+  this.setState({videos: response.data.items, selectedVideo: response.data.items[0]});
+}
+    render(){
+      const { selectedVideo, videos } = this.state;
+
+        return(
+            <Grid style={{ justifyContent: 'center' }} container spacing={10}>
+          <Grid item xs={12}>
+            <Grid container spacing={10}>
+              <Grid item xs={12}>
+             <SearchBar onFormSubmit={this.handleSubmit}/>
+              </Grid>
+              <Grid item xs={8}>
+              	<VideoDetail video={selectedVideo}/>
+              </Grid>
+              <Grid item xs={4}>
+              	<VideoList videos={videos} onVideoSelect={this.onVideoSelect} />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        )
+    }
+}
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//import AppHeader from './AppHeader';
+import App from './App';
+
+
+//ReactDOM.render(<AppHeader />,document.querySelector('#top'));
+ReactDOM.render(<App />, document.querySelector('#root'));
